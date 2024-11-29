6de5b7d7ca35b05ddd5e24b990aa750d09e843da
Murat Görgülü <muratgorgl@gmail.com>
2024-11-29 14:48:27 +0300

    main.py

--- main.py
+++ main.py
@@ -19,25 +19,24 @@ while True:
     results = model(frame)
     print(results)
     human_count = 0
-    # Tespit edilen nesneleri çiz
-    for result in results[0].boxes:  # her bir nesne için (doğru format)
-        x1, y1, x2, y2 = result.xyxy[0].tolist()  # Koordinatları al
-        conf = result.conf[0].item()  # Güven skorunu al
-        cls = result.cls[0].item()  # Sınıf ID'sini al
+ 
+    for result in results[0].boxes:  
+        x1, y1, x2, y2 = result.xyxy[0].tolist()  
+        conf = result.conf[0].item()  
+        cls = result.cls[0].item()  
         
-        if int(cls) == 0:  # Sınıf '0' insanı temsil eder (COCO dataset'inde)
-            # Tespit edilen insanı çerçevele
+        if int(cls) == 0: 
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
             cv2.putText(frame, f"Human {conf:.2f}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 0), 1)
 
             if x1 > p1[0] and y1 < p1[1] and x2 < p2[0] and y2 > p2[1]:
                 human_count+=1 
 
-    # Yarı opak dikdörtgen çizimi
+ 
     overlay = frame.copy()
     opacity = 0.2
     cv2.rectangle(overlay, (200, 720), (1000, 300), (255, 0, 0), thickness=cv2.FILLED)
-    cv2.addWeighted(overlay, opacity, frame, 1 - opacity, 0, frame)  # Yarı opaklık ekleme
+    cv2.addWeighted(overlay, opacity, frame, 1 - opacity, 0, frame)  
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
